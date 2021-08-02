a78b5f53b68483273e64f261b6e23231e5653204
Lucien Ramassamy <lucien.ramassamy@hetic.net>
2021-08-02 14:03:01 +0200

    Feat(Network): add class Network and methods for the movie ds request

--- App.js
+++ App.js
@@ -1,16 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Axios from 'axios'
-
 
 export default function App() {
-
-  Axios('https://api.themoviedb.org/3/movie/12/lists?api_key=c79ee99246533da451af92bf868dc5b2&language=en-US&page=1')
-  .then( response => {
-    console.log(response.data.results);
-    
-  })
 
   return (
     <View style={styles.container}>
---
+++ network.js
@@ -0,0 +1,32 @@
+import Axios from 'axios'
+
+export default class Network {
+  constructor () {
+    this.api_key = 'c79ee99246533da451af92bf868dc5b2'
+  }
+
+  axiosGetRequest(path) {
+    return Axios(path)
+  }
+
+  getMoviesCollections() {
+    this.axiosGetRequest(`https://api.themoviedb.org/3/genre/movie/list?api_key=${this.api_key}&language=fr`)
+      .then( response => {
+        console.log(response.data.genres);
+      })
+  }
+
+  getMoviesListsWithCollectionID(movie_list_id) {
+    this.axiosGetRequest(`https://api.themoviedb.org/3/discover/movie?api_key=${this.api_key}&with_genres=${movie_list_id}&language=fr`)
+      .then(response => {
+        console.log(response.data.results);
+      })
+  }
+
+  getMovie(movie_id) {
+    this.axiosGetRequest(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=${this.api_key}&language=fr`)
+      .then(response => {
+        console.log(response);
+      })
+  }
+}
